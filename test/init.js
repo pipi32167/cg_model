e45a4e2c6a55ce3e3d08e427c0d74f2337fa1d4f
Million Young <pipi32167@gmail.com>
2014-11-21 18:54:07 +0800

    init

---
+++ test/init.js
@@ -0,0 +1,40 @@
+'use strict';
+var mysql = require('mysql');
+var redis = require('redis');
+var model = require('../lib');
+model.debug_mode = true;
+
+module.exports = function(dbConfig) {
+
+  var pool = mysql.createPool(dbConfig || {
+    connectionLimit: 10,
+    host: 'localhost',
+    user: 'yqb',
+    password: 'yqb',
+    database: 'model_test',
+  });
+
+  pool.config.connectionConfig.queryFormat = function(query, values) {
+    if (!values) return query;
+    var res = query
+      .replace(/\$\$(\w+)/g, function(txt, key) {
+        if (values.hasOwnProperty(key)) {
+          return values[key];
+        }
+        return txt;
+      }.bind(this))
+      .replace(/\$(\w+)/g, function(txt, key) {
+        if (values.hasOwnProperty(key)) {
+          return this.escape(values[key]);
+        }
+        return txt;
+      }.bind(this));
+    console.log('sql:', res);
+    return res;
+  };
+
+  model.setDBClient(pool);
+
+  var redisClient = redis.createClient();
+  model.setCacheClient(redisClient);
+}
---
+++ test/test.friend.js
@@ -0,0 +1,66 @@
+'use strict';
+var async = require('async');
+require('./friend');
+require('./init')({
+  connectionLimit: 10,
+  host: 'localhost',
+  user: 'yqb',
+  password: 'yqb',
+  database: 'card',
+});
+var Friend = require('../lib').getModel('Friend');
+
+var testCRUD = function() {
+
+  var friend = new Friend();
+  // var loadUser = new User();
+  // var updateUser = new User();
+  // var removeUser = new User();
+
+  async.series({
+
+    create: function(cb) {
+      console.log('============ create ============');
+      friend.p({
+        userId: 1,
+        friendId: 4,
+        type: 0,
+      });
+      friend.create(cb);
+    },
+
+    // load: function(cb) {
+    //   console.log('============ load ============');
+    //   loadUser.p('userId', user.p('userId'));
+    //   loadUser.load(cb);
+    // },
+
+    // update: function(cb) {
+    //   console.log('============ update ============');
+    //   loadUser.p('name', '0' + loadUser.p('name'));
+    //   loadUser.update(cb);
+    // },
+
+    // load2: function(cb) {
+    //   console.log('============ load2 ============');
+    //   updateUser.p('userId', loadUser.p('userId'));
+    //   updateUser.load(cb);
+    // },
+
+    // remove: function(cb) {
+    //   console.log('============ remove ============');
+    //   loadUser.remove(cb);
+    // },
+
+    // load3: function(cb) {
+    //   console.log('============ load3 ============');
+    //   removeUser.p('userId', loadUser.p('userId'));
+    //   removeUser.load(cb);
+    // }
+  }, function(err) {
+    console.log(err);
+    process.exit(0);
+  })
+}
+
+testCRUD();
